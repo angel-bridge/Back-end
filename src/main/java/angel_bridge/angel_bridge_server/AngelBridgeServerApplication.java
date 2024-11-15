baaca713d69c915a727ef037972c3b609af6de47
choiseoji <choiseoji123@naver.com>
2024-11-15 21:06:42 +0900

    chore: 환경설정 세팅

---
+++ src/main/java/angel_bridge/angel_bridge_server/AngelBridgeServerApplication.java
@@ -0,0 +1,13 @@
+package angel_bridge.angel_bridge_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngelBridgeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngelBridgeServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/angel_bridge/angel_bridge_server/AngelBridgeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package angel_bridge.angel_bridge_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngelBridgeServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
