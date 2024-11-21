d8e1678982a3f97b432bb26763d2f71252033747
Hanseul Lee <lhs010107@gmail.com>
2024-11-22 03:39:39 +0900

    [#3] feat: Member 엔티티 Builder 추가

--- src/main/java/angel_bridge/angel_bridge_server/domain/member/entity/Member.java
+++ src/main/java/angel_bridge/angel_bridge_server/domain/member/entity/Member.java
@@ -3,8 +3,10 @@ package angel_bridge.angel_bridge_server.domain.member.entity;
 import angel_bridge.angel_bridge_server.global.common.BaseEntity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
 
 import java.sql.Date;
 import java.time.LocalDateTime;
@@ -12,6 +14,7 @@ import java.time.LocalDateTime;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@SQLDelete(sql = "UPDATE member SET deleted_at = NOW() where id = ?")
 public class Member extends BaseEntity {
 
     @Id
@@ -25,9 +28,6 @@ public class Member extends BaseEntity {
     @Column(length = 50, nullable = false)
     private String nickname;
 
-    @Column(name = "profile_image")
-    private String profileImage;
-
     @Column(length = 320)
     private String email;
 
@@ -36,13 +36,23 @@ public class Member extends BaseEntity {
 
     @Enumerated(EnumType.STRING)
     @Column(name = "login_type", nullable = false, length = 8)
-    private LoginType loginType = LoginType.KAKAO;
+    private LoginType loginType;
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false, length = 8)
-    private MemberStatus status = MemberStatus.ACTIVE;
+    private MemberStatus status;
 
     @Column(name = "inactive_date")
     private LocalDateTime inactiveDate;
 
+    @Builder
+    public Member(String name, String nickname, String email, String phoneNumber, String loginType, String status, LocalDateTime inactiveDate) {
+        this.name = name;
+        this.nickname = nickname;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.loginType = (loginType == null || loginType.isEmpty()) ? LoginType.KAKAO : LoginType.valueOf(loginType);
+        this.status = (status == null || status.isEmpty()) ? MemberStatus.ACTIVE : MemberStatus.valueOf(status);
+        this.inactiveDate = inactiveDate;
+    }
 }
