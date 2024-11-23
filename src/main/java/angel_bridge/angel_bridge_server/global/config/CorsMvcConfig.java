2e106f6516d882cb1b431939c2de21c925e68dc1
Hanseul Lee <lhs010107@gmail.com>
2024-11-23 22:50:26 +0900

    [#3] feat: CorsMvcConfig 구현

---
+++ src/main/java/angel_bridge/angel_bridge_server/global/config/CorsMvcConfig.java
@@ -0,0 +1,20 @@
+package angel_bridge.angel_bridge_server.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        // TODO: 운영환경에 맞는 보안설정 변경 필요
+
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowCredentials(true)
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
+    }
+}
