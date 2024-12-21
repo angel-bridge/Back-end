4286e16a1778e97a79f65360ec4500c7e99dc300
choiseoji <choiseoji123@naver.com>
2024-12-22 01:05:58 +0900

    [#9] feat: EducationController에 swagger 설명 추가

--- src/main/java/angel_bridge/angel_bridge_server/domain/education/controller/EducationController.java
+++ src/main/java/angel_bridge/angel_bridge_server/domain/education/controller/EducationController.java
@@ -3,6 +3,8 @@ package angel_bridge.angel_bridge_server.domain.education.controller;
 import angel_bridge.angel_bridge_server.domain.education.dto.RecommendationProgramResponse;
 import angel_bridge.angel_bridge_server.domain.education.service.EducationService;
 import angel_bridge.angel_bridge_server.global.common.response.CommonResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,14 +15,15 @@ import java.util.List;
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api/v1/education")
+@Tag(name = "USER_Education", description = "USER 교육 프로그램 관련 API")
 public class EducationController {
 
     private EducationService educationService;
 
+    @Operation(summary = "추천 프로그램 조회", description = "3개의 추천 프로그램 조회하는 API")
     @GetMapping("/recommendations")
     public CommonResponse<List<RecommendationProgramResponse>> getRecommendationProgram() {
 
-        List<RecommendationProgramResponse> recommendationPrograms = educationService.getRecommendationProgram();
-        return new CommonResponse<>(recommendationPrograms, "추천 프로그램 조회 완료");
+        return new CommonResponse<>(educationService.getRecommendationProgram(), "추천 프로그램 조회에 성공하였습니다.");
     }
 }
