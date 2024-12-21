c5b22f10fb9dec8301195399b22a0e40a2559f77
choiseoji <choiseoji123@naver.com>
2024-12-22 00:59:11 +0900

    [#9] feat: 모든 배너를 반환하는 repository 코드 추가

--- src/main/java/angel_bridge/angel_bridge_server/global/repository/BannerRepository.java
+++ src/main/java/angel_bridge/angel_bridge_server/global/repository/BannerRepository.java
@@ -3,9 +3,11 @@ package angel_bridge.angel_bridge_server.global.repository;
 import angel_bridge.angel_bridge_server.domain.banner.entity.Banner;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface BannerRepository extends JpaRepository<Banner, Long> {
 
     Optional<Banner> findByIdAndDeletedAtIsNull(Long bannerId);
+    List<Banner> findByDeletedAtIsNull();
 }
