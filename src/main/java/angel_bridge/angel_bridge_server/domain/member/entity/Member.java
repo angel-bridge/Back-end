ee0ceb67e3f73fa294795bc8e5e8ebf3a7f44284
Hanseul Lee <lhs010107@gmail.com>
2024-12-22 17:36:42 +0900

    [#14] feat: Member 엔티티 profileImage 필드 추가

--- src/main/java/angel_bridge/angel_bridge_server/domain/member/entity/Member.java
+++ src/main/java/angel_bridge/angel_bridge_server/domain/member/entity/Member.java
@@ -32,6 +32,9 @@ public class Member extends BaseEntity {
     @Column(length = 320)
     private String email;
 
+    @Column(name = "profile_image")
+    private String profileImage;
+
     @Column(name = "phone_number", length = 20)
     private String phoneNumber;
 
@@ -40,7 +43,7 @@ public class Member extends BaseEntity {
     private LoginType loginType;
 
     @Enumerated(EnumType.STRING)
-    @Column(nullable = false, length = 8)
+    @Column(nullable = false, length = 10)
     private MemberStatus status;
 
     @Column
@@ -74,10 +77,11 @@ public class Member extends BaseEntity {
     }
 
     @Builder
-    public Member(String name, String nickname, String email, String phoneNumber, LoginType loginType, MemberStatus status, String role, Boolean isSelect, String oauthname, Boolean isRegistered) {
+    public Member(String name, String nickname, String email, String profileImage, String phoneNumber, LoginType loginType, MemberStatus status, String role, Boolean isSelect, String oauthname, Boolean isRegistered) {
         this.name = name;
         this.nickname = nickname;
         this.email = email;
+        this.profileImage = profileImage;
         this.phoneNumber = phoneNumber;
         this.loginType = (loginType == null ? LoginType.KAKAO : loginType);
         this.status = (status == null ? MemberStatus.ACTIVE : status);
