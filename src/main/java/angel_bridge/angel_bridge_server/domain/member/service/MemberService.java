9fe8e281ece1e7e477883ec1a39f0e2199a58d1a
Hanseul Lee <lhs010107@gmail.com>
2024-12-25 02:37:27 +0900

    [#14] feat: 프로필 이미지 처리 로직 3가지 경우의 수 별 로직 구성

--- src/main/java/angel_bridge/angel_bridge_server/domain/member/service/MemberService.java
+++ src/main/java/angel_bridge/angel_bridge_server/domain/member/service/MemberService.java
@@ -4,6 +4,7 @@ import angel_bridge.angel_bridge_server.domain.member.dto.request.AuthRequestDto
 import angel_bridge.angel_bridge_server.domain.member.dto.request.MemberRequestDto;
 import angel_bridge.angel_bridge_server.domain.member.dto.response.MemberResponseDto;
 import angel_bridge.angel_bridge_server.domain.member.entity.Member;
+import angel_bridge.angel_bridge_server.domain.member.entity.ProfileImageType;
 import angel_bridge.angel_bridge_server.global.exception.ApplicationException;
 import angel_bridge.angel_bridge_server.global.repository.MemberRepository;
 import angel_bridge.angel_bridge_server.global.s3.service.ImageService;
@@ -46,20 +47,68 @@ public class MemberService {
 
         Member member = findMemberById(memberId);
 
-        String imageUrl = member.getProfileImage();
+        String newImage;
 
         try {
-            if (profileImage != null && !profileImage.isEmpty()) {
-                imageUrl = imageService.uploadImage(profileImage);
-            }
+            newImage = updateProfileImage(profileImage, member);
         } catch (IOException e) {
-            log.error("이미지 업로드 중 오류 발생: {}", e.getMessage(), e);
+            log.error("이미지 처리 중 오류 발생: {}", e.getMessage(), e);
             throw new ApplicationException(IMAGE_UPLOAD_ERROR);
         }
 
-        member.update(request, imageUrl);
+        member.update(request, newImage);
         Member updateMember = memberRepository.save(member);
 
         return MemberResponseDto.from(updateMember);
+    }
+
+    /**
+     * 프로필 이미지 처리 로직 (3가지 경우의 수)
+     */
+    private String updateProfileImage(MultipartFile profileImage, Member member) throws IOException {
+
+        // 경우 1 ) 프로필 이미지 아예 삭제하는 경우
+        if (profileImage == null) {
+
+            // 1-1. 카카오 프로필 삭제하는 경우
+            if (member.getImageType().equals(ProfileImageType.KAKAO) || member.getProfileImage() == null) {
+                return null;
+            }
+
+            // 1-2.기존 프로필 사진 삭제하는 경우
+            deleteOldImage(member);
+            return null;
+        }
+
+        // 경우 2 ) 프로필 이미지 수정하는 경우
+        if (!profileImage.isEmpty()) {
+            if (member.getImageType().equals(ProfileImageType.KAKAO) || member.getProfileImage() == null) {
+
+                // 이미지 타입을 ANGEL 로 변환
+                member.updateImageType();
+                memberRepository.save(member);
+
+                // 카카오 이미지 타입은 업로드만 처리
+                return imageService.uploadImage(profileImage);
+            }
+
+            // 기존 이미지를 삭제한 뒤 새 이미지 업로드
+            deleteOldImage(member);
+            return imageService.uploadImage(profileImage);
+        }
+
+        // 경우 3 ) 프로필 이미지 수정하지 않는 경우
+        return member.getProfileImage();
+    }
+
+    /**
+     * 기존 이미지 삭제하는 로직
+     */
+    private void deleteOldImage(Member member) {
+
+        String image = member.getProfileImage();
+        if (image != null && !image.isEmpty()) {
+            imageService.deleteImage(image);
+        }
     }
 }
