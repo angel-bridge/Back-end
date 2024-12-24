29f84e380b9274523a68e9eef4d420537f0a9d9d
choiseoji <choiseoji123@naver.com>
2024-12-25 00:25:34 +0900

    [#22] feat: 모집 중, 모집 예정, 전체 프로그램 조회 컨트롤러 추가

--- src/main/java/angel_bridge/angel_bridge_server/domain/education/controller/EducationController.java
+++ src/main/java/angel_bridge/angel_bridge_server/domain/education/controller/EducationController.java
@@ -1,6 +1,7 @@
 package angel_bridge.angel_bridge_server.domain.education.controller;
 
 import angel_bridge.angel_bridge_server.domain.education.dto.RecommendationProgramResponse;
+import angel_bridge.angel_bridge_server.domain.education.dto.response.ProgramResponseDto;
 import angel_bridge.angel_bridge_server.domain.education.service.EducationService;
 import angel_bridge.angel_bridge_server.global.common.response.CommonResponse;
 import io.swagger.v3.oas.annotations.Operation;
@@ -8,6 +9,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -25,5 +27,26 @@ public class EducationController {
     public CommonResponse<List<RecommendationProgramResponse>> getRecommendationProgram() {
 
         return new CommonResponse<>(educationService.getRecommendationProgram(), "추천 프로그램 조회에 성공하였습니다.");
+    }
+
+    @Operation(summary = "전체 프로그램 조회", description = "전체 프로그램 조회하는 API")
+    @GetMapping
+    public CommonResponse<List<ProgramResponseDto>> getAllProgram(@RequestParam(defaultValue = "0") int page) {
+
+        return new CommonResponse<>(educationService.getAllProgram(page), "전체 프로그램 조회에 성공하였습니다.");
+    }
+
+    @Operation(summary = "모집 중인 전체 프로그램 조회", description = "모집 중인 전체 프로그램 조회하는 API")
+    @GetMapping("/ongoing")
+    public CommonResponse<List<ProgramResponseDto>> getAllOngoingProgram(@RequestParam(defaultValue = "0") int page) {
+
+        return new CommonResponse<>(educationService.getAllOngoingProgram(page), "모집 중인 전체 프로그램 조회에 성공하였습니다.");
+    }
+
+    @Operation(summary = "모집 예정인 전체 프로그램 조회", description = "모집 예정인 전체 프로그램 조회하는 API")
+    @GetMapping("/upcoming")
+    public CommonResponse<List<ProgramResponseDto>> getAllUpcomingProgram(@RequestParam(defaultValue = "0") int page) {
+
+        return new CommonResponse<>(educationService.getAllUpcomingProgram(page), "모집 예정인 전체 프로그램 조회에 성공하였습니다.");
     }
 }
