f339f131f58825e33643fc675dc8711c3ee2be8a
choiseoji <choiseoji123@naver.com>
2024-12-26 18:03:19 +0900

    [#22] feat: ProgramResponse DTO에 설명과 사진 url을 주입받도록 추가

--- src/main/java/angel_bridge/angel_bridge_server/domain/education/dto/response/ProgramResponseDto.java
+++ src/main/java/angel_bridge/angel_bridge_server/domain/education/dto/response/ProgramResponseDto.java
@@ -1,24 +1,32 @@
 package angel_bridge.angel_bridge_server.domain.education.dto.response;
 
 import angel_bridge.angel_bridge_server.domain.education.entity.Education;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 @Builder
 public record ProgramResponseDto(
+
+        @Schema(description = "프로그램 id", example = "1")
         Long educationId,
 
+        @Schema(description = "프리뷰 이미지", example = "프리뷰 이미지 url")
         String preImage,
 
+        @Schema(description = "프로그램 소개", example = "21일 간 9개의 미션 수행 및 피드백")
         String description,
 
+        @Schema(description = "제목", example = "예비 창업 패키지 2주 완성")
         String title,
+
+        @Schema(description = "모집 여부", example = "모집중")
         String recruitmentStatus
 ) {
 
-    public static ProgramResponseDto from(Education education) {
+    public static ProgramResponseDto from(Education education, String preImage) {
         return ProgramResponseDto.builder()
                 .educationId(education.getId())
-                .preImage(education.getEducationPreImage())
+                .preImage(preImage)
                 .description(education.getEducationDescription())
                 .title(education.getEducationTitle())
                 .recruitmentStatus(education.getRecruitmentStatus().getDescription())
