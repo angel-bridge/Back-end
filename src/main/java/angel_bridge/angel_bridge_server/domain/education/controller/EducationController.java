3946a687b817041da295a2cf6500db382f02ef6a
Hanseul Lee <lhs010107@gmail.com>
2024-12-27 22:23:52 +0900

    [#27] feat: [GET] 프로그램 검색 API 구현

--- src/main/java/angel_bridge/angel_bridge_server/domain/education/controller/EducationController.java
+++ src/main/java/angel_bridge/angel_bridge_server/domain/education/controller/EducationController.java
@@ -5,7 +5,10 @@ import angel_bridge.angel_bridge_server.domain.education.dto.response.EducationR
 import angel_bridge.angel_bridge_server.domain.education.service.EducationService;
 import angel_bridge.angel_bridge_server.global.common.response.CommonResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -28,21 +31,21 @@ public class EducationController {
 
     @Operation(summary = "전체 프로그램 조회", description = "전체 프로그램 조회하는 API")
     @GetMapping
-    public CommonResponse<List<EducationResponseDto>> getAllProgram(@RequestParam(defaultValue = "0") int page) {
+    public CommonResponse<List<EducationResponseDto>> getAllProgram(@RequestParam(defaultValue = "1") int page) {
 
         return new CommonResponse<>(educationService.getAllProgram(page), "전체 프로그램 조회에 성공하였습니다.");
     }
 
     @Operation(summary = "모집 중인 전체 프로그램 조회", description = "모집 중인 전체 프로그램 조회하는 API")
     @GetMapping("/ongoing")
-    public CommonResponse<List<EducationResponseDto>> getAllOngoingProgram(@RequestParam(defaultValue = "0") int page) {
+    public CommonResponse<List<EducationResponseDto>> getAllOngoingProgram(@RequestParam(defaultValue = "1") int page) {
 
         return new CommonResponse<>(educationService.getAllOngoingProgram(page), "모집 중인 전체 프로그램 조회에 성공하였습니다.");
     }
 
     @Operation(summary = "모집 예정인 전체 프로그램 조회", description = "모집 예정인 전체 프로그램 조회하는 API")
     @GetMapping("/upcoming")
-    public CommonResponse<List<EducationResponseDto>> getAllUpcomingProgram(@RequestParam(defaultValue = "0") int page) {
+    public CommonResponse<List<EducationResponseDto>> getAllUpcomingProgram(@RequestParam(defaultValue = "1") int page) {
 
         return new CommonResponse<>(educationService.getAllUpcomingProgram(page), "모집 예정인 전체 프로그램 조회에 성공하였습니다.");
     }
@@ -52,5 +55,18 @@ public class EducationController {
     public CommonResponse<EducationDetailResponseDto> getEducationDetail(@PathVariable Long educationId) {
 
         return new CommonResponse<>(educationService.getEducationDetail(educationId), "프로그램 상세 페이지 조회에 성공하였습니다.");
+    }
+
+    @Operation(summary = "프로그램 검색", description = "프로그램 이름 기준 검색하는 API")
+    @GetMapping("/search")
+    public CommonResponse<List<EducationResponseDto>> searchEducationByTitle(@RequestParam String keyword, @RequestParam(defaultValue = "1") int page, @Parameter(
+            examples = {
+                    @ExampleObject(name = "'전체'에서 검색", value = "ALL"),
+                    @ExampleObject(name = "'모집중'에서 검색", value = "ONGOING"),
+                    @ExampleObject(name = "'모집예정'에서 검색", value = "UPCOMING")
+            }
+    ) @RequestParam(defaultValue = "ALL") String status) {
+
+        return new CommonResponse<>(educationService.searchEducationByTitle(keyword, page, status), "해당 keyword에 대한 교육 프로그램 조회에 성공하였습니다.");
     }
 }
