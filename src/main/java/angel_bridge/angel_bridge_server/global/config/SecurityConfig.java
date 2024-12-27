2bd1dad8dc2cffc789a6d2c18eabb27e44c0f307
Hanseul Lee <lhs010107@gmail.com>
2024-12-28 00:13:45 +0900

    [#27] refactor: 응답 반환 dto 설명 추가

--- src/main/java/angel_bridge/angel_bridge_server/global/config/SecurityConfig.java
+++ src/main/java/angel_bridge/angel_bridge_server/global/config/SecurityConfig.java
@@ -92,7 +92,6 @@ public class SecurityConfig {
                         // 임시
                         .requestMatchers("/api/v1/admin/**").hasRole("ADMIN")
                         .requestMatchers("/api/v1/**").permitAll()
-                        .requestMatchers("/api/auth/reissue").permitAll()
                         .requestMatchers("/hc", "/env", "/").permitAll()
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated());
