d24b65d404e0f1be1ef94180c477c8ac3cf1d08a
choiseoji <choiseoji123@naver.com>
2025-01-07 01:15:30 +0900

    [#33] feat: findByEnrollment에 member 추가

--- src/main/java/angel_bridge/angel_bridge_server/global/repository/EnrollmentRepository.java
+++ src/main/java/angel_bridge/angel_bridge_server/global/repository/EnrollmentRepository.java
@@ -2,6 +2,7 @@ package angel_bridge.angel_bridge_server.global.repository;
 
 import angel_bridge.angel_bridge_server.domain.enrollment.entity.Enrollment;
 import angel_bridge.angel_bridge_server.domain.enrollment.entity.EnrollmentStatus;
+import angel_bridge.angel_bridge_server.domain.member.entity.Member;
 import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.domain.Pageable;
@@ -11,7 +12,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 public interface EnrollmentRepository extends JpaRepository<Enrollment, Long> {
 
-    Page<Enrollment> findByEnrollmentStatusAndDeletedAtIsNull(EnrollmentStatus status, Pageable pageable);
+    Page<Enrollment> findByMemberAndEnrollmentStatusAndDeletedAtIsNull(Member member, EnrollmentStatus enrollmentStatus, Pageable pageable);
 
     @Modifying
     @Transactional
