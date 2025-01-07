4eb2e5a54438b29f213339eaeff57fe811bf24e8
choiseoji <choiseoji123@naver.com>
2025-01-08 02:08:38 +0900

    [#32] feat: 현재 시점 기준 수강예정, 수강중, 수강완료 값 계산해서 반환하는 메서드 추가

--- src/main/java/angel_bridge/angel_bridge_server/domain/enrollment/entity/EnrollmentStatus.java
+++ src/main/java/angel_bridge/angel_bridge_server/domain/enrollment/entity/EnrollmentStatus.java
@@ -3,6 +3,8 @@ package angel_bridge.angel_bridge_server.domain.enrollment.entity;
 import lombok.Getter;
 import lombok.ToString;
 
+import java.time.LocalDate;
+
 @Getter
 @ToString
 public enum EnrollmentStatus {
@@ -15,5 +17,17 @@ public enum EnrollmentStatus {
 
     EnrollmentStatus(String description) {
         this.description = description;
+    }
+
+    public static EnrollmentStatus getStatusFromEducationStartDate(
+            LocalDate educationStartDate,
+            LocalDate educationEndDate) {
+
+        LocalDate now = LocalDate.now();
+        if (now.isBefore(educationStartDate))
+            return SCHEDULED;
+        if (now.isAfter(educationEndDate))
+            return COMPLETED;
+        return IN_PROGRESS;
     }
 }
