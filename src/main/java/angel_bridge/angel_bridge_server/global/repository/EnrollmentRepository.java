a4076cf69b1664c58e8965a59916a00e7a67e216
choiseoji <choiseoji123@naver.com>
2025-01-08 11:34:44 +0900

    [#32] feat: 결제 완료, 취소 조회 repository 코드 추가

--- src/main/java/angel_bridge/angel_bridge_server/global/repository/EnrollmentRepository.java
+++ src/main/java/angel_bridge/angel_bridge_server/global/repository/EnrollmentRepository.java
@@ -2,12 +2,15 @@ package angel_bridge.angel_bridge_server.global.repository;
 
 import angel_bridge.angel_bridge_server.domain.enrollment.entity.Enrollment;
 import angel_bridge.angel_bridge_server.domain.enrollment.entity.EnrollmentStatus;
+import angel_bridge.angel_bridge_server.domain.member.entity.Member;
 import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 public interface EnrollmentRepository extends JpaRepository<Enrollment, Long> {
 
@@ -22,4 +25,7 @@ public interface EnrollmentRepository extends JpaRepository<Enrollment, Long> {
     @Transactional
     @Query("UPDATE Enrollment e SET e.enrollmentStatus = 'COMPLETED' WHERE e.education.educationEndDate <= CURRENT_DATE AND e.enrollmentStatus = 'IN_PROGRESS'")
     void updateEnrollmentStatusToCompleted();
+
+    List<Enrollment> findByMemberAndDeletedAtIsNull(Member member);
+    List<Enrollment> findByMemberAndDeletedAtIsNotNull(Member member);
 }
