5e68b01a89a2cce44ffed7973d42081273babb0e
choiseoji <choiseoji123@naver.com>
2025-01-17 21:13:11 +0900

    [#41] refactor: Canceled, CompletePaymentResponseDto를 PaymentResponseDto로 합침

--- src/main/java/angel_bridge/angel_bridge_server/domain/payment/dto/response/PaymentResponseDto.java
+++ src/main/java/angel_bridge/angel_bridge_server/domain/payment/dto/response/PaymentResponseDto.java
@@ -1,11 +1,17 @@
 package angel_bridge.angel_bridge_server.domain.payment.dto.response;
 
 import lombok.Builder;
+
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Builder
 public record PaymentResponseDto(
-        List<CompletePaymentResponseDto> complete,
-        List<CanceledPaymentResponseDto> canceled
+        Long enrollmentId,
+        String imageUrl,
+        String educationName,
+        String price,
+        LocalDateTime date,
+        String status
 ) {
 }
